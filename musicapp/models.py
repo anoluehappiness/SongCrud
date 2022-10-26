193aad2c5d47c895f6bd0ad6976b5460f4b89293
unknown <happinessanolue@gmail.com>
2022-10-26 23:19:02 +0100

    week_5 assignment commit

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,20 @@
+from email.policy import default
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    date_released = models.DateField(default=datetime)
+    likes = models.IntegerField()
+    
+    
+class Lyric(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
